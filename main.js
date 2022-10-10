7eb2ed589517af4dc6d8b05c3b17d6b742bb7abd
unknown <sd979141@gmail.com>
2022-10-10 21:02:06 +0900

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+function menu(){
+    $(`.nav`).toggleClass(`active`);
+    $(`.nav i`).toggleClass(`active`);
+    $(`.nav ul`).toggleClass(`active`);
+    $(`.fullpage`).toggleClass(`active`)
+};
+function user(){
+    $(`.nav-right ul`).toggleClass(`active`);
+}
